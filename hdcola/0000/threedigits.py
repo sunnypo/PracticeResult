623cd6c69581c3ae5caed416f145d7dbdd5bfc3f
hdcola <hdcola@gmail.com>
2019-06-30 18:36:04 -0400

    add 0000 Practice

---
+++ hdcola/0000/threedigits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+digits = [1,2,3,4]
+
+count = 0
+
+for d1 in digits:
+    for d2 in digits:
+        for d3 in digits:
+            if d1==d2 or d1==d3 or d2==d3 :
+                break
+            print (d1*100+d2*10+d3)
+            count += 1
+
+print("count:%d" % count)
